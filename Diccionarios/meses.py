ad6f8cf6ce1591af9f987b43cb4605ae0f7475c1
Juan Diego Rivera Meneses <jdrivera847@misena.edu.co>
2022-05-17 23:01:19 -0500

    ejercicio_meses

--- Diccionarios/meses.py
+++ Diccionarios/meses.py
@@ -1,20 +1,34 @@
+produccion= dict(
+    Enero=35627,
+    febrero=567483,
+    marzo=64738,
+    abril=6547382,
+    mayo=564738,
+    junio=637282,
+    julio=46778,
+    agosto=6929,
+    septiembre=89234,
+    octubre=67292,
+    noviembre=81239,
+    diciembre=89,
+)
+llave_min=min(produccion.keys(), key=lambda k:produccion[k])
+print('El mes de menor produccion fue:',llave_min,produccion[llave_min])
 
+llave_max=max(produccion.keys(), key=lambda k:produccion[k])
+print('El mes de mayor produccion fue:',llave_max,produccion[llave_max])
 
+prom=tuple(produccion.values())
+b=len(prom)
+suma=0
+for valor in prom:
+    suma+=valor
+print('El promedio es:',suma/b)
 
-
-
-
-
-
-
-
-print(f"El mes con menor produccion es: {minimo}")
-print(f"el promedio es: {promedio}")
-
-for key in meses.keys():
-    if (meses[key]>=promedio):
+for key in produccion.keys():
+    if(produccion[key]>=prom):
         
-        print("el mes",key,"es mayor que el promedio")
-        
-    elif (meses[key]<=promedio):
-        print(f"el mes: {key}, es menor que el promedio")
+     print('el mes',(key),'es mayor que el promedio')
+    
+    elif (produccion[key]<=prom):
+        print(f'el mes:', {key}, 'es menor que el promedio')
